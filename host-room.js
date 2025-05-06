841ed0b3a89fef5dae0d028922ba002bfcd4a395
Ashika Benson <ashikathankam.benson@yatnam.com>
2025-05-06 16:36:33 +0530

    #1 Tic tac toe style and 2 modes working

---
+++ host-room.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    // This file is a placeholder for future implementation of the host room functionality
+    // Currently it just shows the "Coming Soon" modal
+    
+    // You could add a timer to redirect back to home after a few seconds
+    // Uncomment the following code if you want to automatically redirect
+    /*
+    setTimeout(function() {
+        window.location.href = 'index.html';
+    }, 5000); // Redirect after 5 seconds
+    */
+};
